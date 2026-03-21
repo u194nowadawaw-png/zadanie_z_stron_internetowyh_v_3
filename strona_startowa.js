26fd555c4f917f44b3de662d684d68a96a8d6534
adasn290-byte <adasn290@gmail.com>
2026-03-21 20:30:27 +0100

    zegar

--- strona_startowa.js
+++ strona_startowa.js
@@ -16,3 +16,23 @@ document.body.addEventListener("click", function() {
         void h1.offsetWidth; 
         h1.classList.add("obrot");
     });
+function pokazGodzine() {
+    const teraz = new Date();
+    
+    let godzina = teraz.getHours();
+    let minuty = teraz.getMinutes();
+    let sekundy = teraz.getSeconds();
+
+    godzina = godzina < 10 ? "0" + godzina : godzina;
+    minuty = minuty < 10 ? "0" + minuty : minuty;
+    sekundy = sekundy < 10 ? "0" + sekundy : sekundy;
+
+    document.getElementById("zegar").innerText = 
+        godzina + ":" + minuty + ":" + sekundy;
+}
+
+
+setInterval(pokazGodzine, 1000);
+
+
+pokazGodzine();
