b2ba02a7efdb6d797118abf1f47d5831e6d18b77
adasn290-byte <adasn290@gmail.com>
2026-03-21 20:11:55 +0100

    nie dzała krencćy się napis

--- strona_startowa.js
+++ strona_startowa.js
@@ -1,7 +1,18 @@
 const background = document.querySelector("#background");
 
-background.addEventListener("click", (event) => {
-  if (event.target === background) {
+
+document.body.addEventListener("click", function() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
     
-  }
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 });
+
+    const h1 = document.getElementById("tytul");
+
+    h1.addEventListener("click", function() {
+        h1.classList.remove("obrot");
+        void h1.offsetWidth; 
+        h1.classList.add("obrot");
+    });
